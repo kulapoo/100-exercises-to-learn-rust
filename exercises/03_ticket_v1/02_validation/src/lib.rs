3b43ff32849674267e75ad41824fcc8fd2e5b2a3
kulapoo <kulaskulapoo@gmail.com>
2024-09-17 23:49:41 +0800

    finsihed validation exercise - (doing rust exercise atleast 1 each day)

--- exercises/03_ticket_v1/02_validation/src/lib.rs
+++ exercises/03_ticket_v1/02_validation/src/lib.rs
@@ -17,11 +17,29 @@ impl Ticket {
     // as well as some `String` methods. Use the documentation of Rust's standard library
     // to find the most appropriate options -> https://doc.rust-lang.org/std/string/struct.String.html
     fn new(title: String, description: String, status: String) -> Self {
-        todo!();
+        if title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+
+        if title.capacity() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+
+        if description.is_empty() {
+            panic!("Description cannot be empty");
+        }
+
+        if description.capacity() > 500 || description.is_empty() {
+            panic!("Description cannot be longer than 500 bytes");
+        }
+
         Self {
             title,
             description,
-            status,
+            status: match status.as_str() {
+                "To-Do" | "In Progress" | "Done" => status,
+                _ => panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed"),
+            },
         }
     }
 }
